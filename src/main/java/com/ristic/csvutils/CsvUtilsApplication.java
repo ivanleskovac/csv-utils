484d0ab8cca58ffcf8991ea95f8db9a72cd2b86d
Ivan Ristic <ivanrisss@gmail.com>
2021-10-09 12:36:45 +0200

    SpringBoot application init commit.

---
+++ src/main/java/com/ristic/csvutils/CsvUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.ristic.csvutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ristic/csvutils/CsvUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ristic.csvutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
