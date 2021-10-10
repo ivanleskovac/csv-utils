235971d9410b1a24b78a5ff1699958fd257147a7
Ivan Ristic <ivanrisss@gmail.com>
2021-10-10 16:56:27 +0200

    Resolved issues with CSV processing by using org.apache.commons:commons-csv library.

---
+++ src/main/java/com/ristic/csvutils/domain/CsvHeaderEnum.java
@@ -0,0 +1,21 @@
+package com.ristic.csvutils.domain;
+
+public enum CsvHeaderEnum {
+	
+	source,
+	
+	codeListCode,
+	
+	code,
+	
+	displayValue,
+	
+	longDescription,
+	
+	fromDate,
+	
+	toDate,
+	
+	sortingPriority
+
+}
--- src/main/java/com/ristic/csvutils/service/impl/CsvUtilsServiceImpl.java
+++ src/main/java/com/ristic/csvutils/service/impl/CsvUtilsServiceImpl.java
@@ -1,21 +1,24 @@
 package com.ristic.csvutils.service.impl;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.UUID;
 
+import org.apache.commons.csv.CSVFormat.Builder;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ristic.csvutils.domain.CsvHeaderEnum;
 import com.ristic.csvutils.domain.CsvRecord;
 import com.ristic.csvutils.repository.CsvRecordsRepository;
 import com.ristic.csvutils.service.CsvUtilsService;
@@ -23,79 +26,55 @@ import com.ristic.csvutils.service.CsvUtilsService;
 @Service
 public class CsvUtilsServiceImpl implements CsvUtilsService {
 
-	private final static String COMMA_DELIMITER = ",";
-	
 	@Autowired
 	CsvRecordsRepository repository;
 
 	@Override
 	public void upload(MultipartFile multipartFile) {
-		List<List<String>> records = readCsvFile(multipartFile);
-		records.remove(0);
+		Iterable<CSVRecord> records = null;
+		try {
+			records = readCsvFile(multipartFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		List<CsvRecord> csvRecords = new ArrayList<>();
 		records.forEach(record -> csvRecords.add(convertToCsvRecord(record)));
 		repository.saveAll(csvRecords);
 	}
 
-	private CsvRecord convertToCsvRecord(List<String> record) {
-		CsvRecord csvRecord = new CsvRecord();
-		csvRecord.setSource(record.get(0));
-		csvRecord.setCodeListCode(record.get(1));
-		csvRecord.setCode(record.get(2));
-		csvRecord.setDisplayValue(record.get(3));
-		csvRecord.setLongDescription(record.get(4));
-		// TODO: Issue with empty fields.
-		// if (!StringUtils.isEmpty(record.get(5))) {
-		// 	csvRecord.setFromDate(LocalDate.parse(record.get(5), DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-		// }
-		// if (!StringUtils.isEmpty(record.get(6))) {
-		// 	csvRecord.setToDate(LocalDate.parse(record.get(6), DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-		// }
-		// csvRecord.setSortingPriority(Integer.valueOf(record.get(7)));
-		return csvRecord;
-	}
-
-	/**
-	 * Read CSV file into list of records (reference from baeldung.com).
-	 * 
-	 * @param multipartFile
-	 * @return list of records
-	 */
-	private List<List<String>> readCsvFile(MultipartFile multipartFile) {
-		List<List<String>> records = new ArrayList<>();
-		File file = new File(System.getProperty("java.io.tmpdir")+"/file.tmp");
-		try {
-			multipartFile.transferTo(file);
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try (Scanner scanner = new Scanner(file)) {
-			while (scanner.hasNextLine()) {
-				records.add(getRecordFromLine(scanner.nextLine()));
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+	private Iterable<CSVRecord> readCsvFile(MultipartFile multipartFile) throws IOException {
+		File file = convertMultipartFile(multipartFile);
+		Reader reader = new FileReader(file);
+		Builder builder = Builder.create().setHeader(CsvHeaderEnum.class);
+		builder.setSkipHeaderRecord(true);
+	    Iterable<CSVRecord> records = builder.build().parse(reader);
 		return records;
 	}
+	
+	private File convertMultipartFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(System.getProperty("java.io.tmpdir")+"/file-" + UUID.randomUUID() + ".tmp");
+        multipartFile.transferTo(file);
+        return file;
+	}
 
-	/**
-	 * Scanning single line (reference from baeldung.com).
-	 * 
-	 * @param line
-	 * @return content of a single line
-	 */
-	private List<String> getRecordFromLine(String line) {
-		List<String> values = new ArrayList<String>();
-		try (Scanner rowScanner = new Scanner(line)) {
-			rowScanner.useDelimiter(COMMA_DELIMITER);
-			while (rowScanner.hasNext()) {
-				values.add(rowScanner.next());
-			}
+	private CsvRecord convertToCsvRecord(CSVRecord record) {
+		CsvRecord csvRecord = new CsvRecord();
+		csvRecord.setSource(record.get(CsvHeaderEnum.source));
+		csvRecord.setCodeListCode(record.get(CsvHeaderEnum.codeListCode));
+		csvRecord.setCode(record.get(CsvHeaderEnum.code));
+		csvRecord.setDisplayValue(record.get(CsvHeaderEnum.displayValue));
+		csvRecord.setLongDescription(record.get(CsvHeaderEnum.longDescription));
+		if (StringUtils.isNotEmpty(record.get(CsvHeaderEnum.fromDate))) {
+			csvRecord.setFromDate(LocalDate.parse(record.get(CsvHeaderEnum.fromDate), DateTimeFormatter.ofPattern("dd-MM-yyyy")));
 		}
-		return values;
+		if (StringUtils.isNotEmpty(record.get(CsvHeaderEnum.toDate))) {
+			csvRecord.setToDate(LocalDate.parse(record.get(CsvHeaderEnum.toDate), DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+		}
+		if (NumberUtils.isParsable(record.get(CsvHeaderEnum.sortingPriority))) {
+			csvRecord.setSortingPriority(Integer.valueOf(record.get(CsvHeaderEnum.sortingPriority)));
+		}
+		return csvRecord;
 	}
 
 	@Override
